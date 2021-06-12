9106f67cf4ae805fe045a29c37e9c0663e9ddc86
cloudblockandbeyond <cloudblockandbeyond@gmail.com>
2021-06-12 22:17:11 +0800

    Exercise 1 - Passing Data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useReducer } from "react";
 import './App.css';
 import logo from './logo.svg';
 import ListUsers from "./ListUsers.js";
@@ -38,78 +38,83 @@ const profiles = [
   },
 ];
 
-const users = [
-  {
+const users = {
+  1: {
     id: 1,
     name: 'Jane Cruz',
     userName: 'coder',
   },
-  {
+  2: {
     id: 2,
     name: 'Matthew Johnson',
     userName: 'mpage',
   },
-  {
+  3: {
     id: 3,
     name: 'Autumn Green',
     userName: 'user123',
   },
-  {
+  4: {
     id: 4,
     name: 'John Doe',
     userName: 'user123',
   },
-  {
+  5: {
     id: 5,
     name: 'Lauren Carlson',
     userName: 'user123',
   },
-  {
+  6: {
     id: 6,
     name: 'Nicholas Lain',
     userName: 'user123',
   },
-];
+};
 
-const movies = [
-  {
+const movies = {
+  1: {
     id: 1,
     name: 'Planet Earth 1',
   },
-  {
+  2: {
     id: 2,
     name: 'Selma',
   },
-  {
+  3: {
     id: 3,
     name: 'Million Dollar Baby',
   },
-  {
+  4: {
     id: 4,
     name: 'Forrest Gump',
   },
-  {
+  5: {
     id: 5,
     name: 'Get Out',
   },
-];
+};
 
 class App extends React.Component{
-	render(){
-    	return(
-          	<div>
-              <header className="App-header">
-                  <img src={logo} className="App-logo" alt="ReactLogo" />
-                  <h1 className="App-title">ReactND - Coding Practice</h1>
-              </header>
-			  <h2>Favorite Movies</h2>
-              <ListUsers users={users} />  
-              <br />
-              <ListMovies movies={movies} />
-              <br />
-			  <ListProfiles profiles={profiles} />
-			</div>
-        );
+	render() {
+    	return (
+        <div>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="ReactLogo" />
+            <h1 className="App-title">ReactND - Coding Practice</h1>
+          </header>
+          <h2>Favorite Movies</h2>
+          { profiles.map((profile) => {
+            const userName = users[profile.userID].name;
+            const movieName = movies[profile.favoriteMovieID].name;
+
+            return (
+              <li key={ profile.id }>
+                {`${userName}'s favorite movie is ${movieName}.`}
+              </li>
+            );
+          }) }
+        </div>
+      );
     }
 }
 
